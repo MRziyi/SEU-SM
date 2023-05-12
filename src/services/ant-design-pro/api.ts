1f4eedf2b0e2f3f55520f763e5f8c6cd8b5291e7
“MRziyi” <357408195@qq.com>
2023-05-12 22:34:38 +0800

    try to link to BE

--- src/services/ant-design-pro/api.ts
+++ src/services/ant-design-pro/api.ts
@@ -22,7 +22,8 @@ export async function outLogin(options?: { [key: string]: any }) {
 
 /** 登录接口 POST /api/login/account */
 export async function login(body: API.LoginParams, options?: { [key: string]: any }) {
-  return request<API.LoginResult>('/api/login/account', {
+  console.log('Request sent!');
+  return request<API.LoginResult>('/api/user/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
